a7d0f73d81dd18e3e612ba068cf637a6f25e0739
Aleksandra Chernova <shuraorgm@gmail.com>
2023-09-05 23:38:19 +0300

    129. Добавление уникального айди к элементам списка - key

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -4,14 +4,16 @@ import EmployersListItem from "../employers-list-item/employers-list-item";
 const EmployersList = ({data}) => {
 
     const elements = data.map(item => {
+        const {id, ...itemProps} = item;
         return (
             // <EmployersListItem name={item.name} salary={item.salary}/>
             // с использование обжект спред оператор
-            <EmployersListItem {...item}/>
+            <EmployersListItem key={id} {...itemProps}/>
 
             // консоль выдает ошибку "Warning: Each child in a list should have a unique "key" prop." но ее позже будем разбирать
         )
     }) 
+
 
     return (
         <ul className="app-list list-group">
